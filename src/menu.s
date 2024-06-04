2706c9f6f130af357a4c3674045b4970e4915bfb
daniel <bastardodentropower@gmail.com>
2024-06-04 18:46:56 +0200

    spostamento file committati male

--- src/menu.s
+++ src/menu.s
@@ -1,56 +1,52 @@
-
 .section .data                 
 
 menu_prompt:
-      .ascii "decidi l'ordine da usare. (1: EDF (scadenza), 2: HPF (priorità), altro: EXIT)"
+    .ascii "decidi l'ordine da usare. (1: EDF (scadenza), 2: HPF (priorità), altro: EXIT):" 
 
 menu_prompt_len:
-      .long . - menu_prompt
+    .long . - menu_prompt
 
-buffer:
-      .byte 0
+scelta:
+	.ascii "00"
+scelta_len:
+	.long . - scelta 
+
+num_int:
+	.long 0
 
 .section .text
-      .global _menu_start
+    .global _start
 
-_menu_start:
-      # PRINT menu_prompt
-      movl $4, %eax
-      movl $1, %ebx
-      leal menu_prompt, %ecx
-      movl hello_len, %edx
-      int $0x80
+_start:
+stampa_richiesta:
+    movl $4, %eax
+    movl $1, %ebx
+    leal menu_prompt, %ecx
+    movl menu_prompt_len, %edx
+    int $0x80
 
-      # READ char
-      movl $3, %eax
-      movl $0, %ebx
-      leal buffer, %ecx
-      movl $1, %edx
-      int $0x80
+scanf:
+    movl $3, %eax
+    movl $1, %ebx
+    leal scelta, %ecx
+    movl scelta_len, %edx
+    int $0x80 
 
-      # MOVE char
-      xor %eax, %eax
-      mov buffer, %al
-      
-      # IF char == '1'
-      movl $49, %ebx,
-      cmp %eax, %ebx
-      je equal_1 
-      
-      # IF char == '2'
-      movl $50, %ebx,
-      cmp %eax, %ebx
-      je equal_2
-      
-      # ELSE EXIT
-      movl $0, %eax
-      movl $0, %ebx
-      int $0x80
+converti:
+	leal scelta, %esi 
+	xorl %eax, %eax
+	xorl %ebx, %ebx
+	xorl %ecx, %ecx
+	xorl %edx, %edx
 
-equal_1:
-      movl $1, %eax
-      ret
+	movb (%ecx,%esi,1), %bl
+	subb $48, %bl 
+	movl $10, %edx 
+	mulb %dl 
+	addl %ebx, %eax
 
-equal_2:
-      movl $2, %eax
-      ret
+	movl %eax, num_int
+
+fine:
+	movl $1, %eax
+	int $0x80
