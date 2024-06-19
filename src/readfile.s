160e25b51a2c69e67da07b81eb3753fe6cbbf40f
Luca Grella <luca.grella.46.lg@gmail.com>
2024-06-19 22:39:38 +0200

    READFILE FUNZIONA

--- src/readfile.s
+++ src/readfile.s
@@ -1,93 +1,102 @@
 .section .data
+    .global number_array
 
-filename: .string "ordini.txt"
+filename: .string "/home/pg/Documents/progettoassembly/elaborato-assembly/bin/ordini.txt"
 fd: .int 0
-buffer: .byte 0
-
-number_index: .int 0
-number_index_max: .int 40 // max 40 numeri (10 prod * 4 campi)
-number_array: .fill 40 // 40 byte
+buffer: .fill 132 # len "ddd,dd,ddd,d\n" * 10 +EOF?+EOF
+number_array: .fill 40
+ten: .byte 10
 
 .section .text
     .global _start
 
-_open:
-    // SYSCALL open (filename, readonly) -> [eax: fd] 
+_start:
+    # syscall open(filename, readonly) -> [eax: fd] 
     mov $5, %eax
-    mov $filename, %ebx
+    leal filename, %ebx
     mov $0, %ecx
     int $0x80
 
-    // if eax == NULL { exit }
+    # IF eax == (NULL | error) { exit }
     cmp $0, %eax
     jl _exit
 
-    // fd = eax
+    # fd = eax
     mov %eax, fd
 
-    // number_index = 0
-    mov $0, number_index
-
-_read_char_loop:
-    // SYSCALL read (fd, *buffer, len=1)
+    # syscall read (fd, *buffer, len=1)
     mov $3, %eax
     mov fd, %ebx
-    mov $buffer, %ecx
-    mov $1, %edx
+    leal buffer, %ecx
+    mov $131, %edx
     int $0x80
 
-    // if (len_bytes <= 1) { _close_file }
-    cmp $0, %eax
-    jle _close_file     // Se ci sono errori o EOF, chiudo il file
-
-    // if (buffer == '\n' | buffer == ',') { numbe_index += 1 }
-    movb buffer, %al
-    cmpb $10, %al
-    jne _check_digit
-    cmpb $44, %al
-    jne _check_digit
-
-    inc number_index
-
-    // else { _check_digit }
-_check_digit:
-    // if (!buffer.is_digit) { panic }
-    sub $48, %al
-    cmpb $10, %al
-    jge _panic
-
-    // array[index] *= 10
-    mov number_index, %ebx
-    movb number_array(%ebx,1), %cl
-    imul $10, %cl
-    add %al, %cl
-    movb %cl, number_array(%ebx,1)
-
-    jmp _read_char_loop
-
-_close_file:
-    // SYSCALL close (fd)
+    # syscall close (fd)
     mov $6, %eax
     mov fd, %ebx
     int $0x80
 
+    # c = 0, i = 0
+    xor %ecx, %ecx
+    xor %edx, %edx
+
+    mov $0, %eax
+
+    leal buffer, %esi
+    leal number_array, %edi
+
+_for_each_char:
+    # bl = char[c]
+    movb (%esi,%ecx,1), %bl
+    # c += 1
+    incl %ecx
+
+    # IF bl == (',' | '\n' | EOF) { next_array_pos }
+    # il 132esimo e' assicurato essere 0 perche' ne leggo 131
+    cmpb $44, %bl
+    je _next_array_pos
+    cmpb $10, %bl
+    je _next_array_pos
+    cmpb $0, %bl
+    je _next_array_pos
+
+    # IF (!bl.is_digit) { panic }
+    sub $48, %bl
+    cmpb $10, %bl
+    jge _panic
+
+    # al *= 10
+    mulb ten
+    addb %bl, %al
+
+    jmp _for_each_char
+
+_next_array_pos:
+    # number[i] = al
+    movb %al, (%edi,%edx,1)
+
+    cmp $40, %edx
+    je _exit
+    cmp $0, %bl
+    je _exit
+
+    # reset eax
+    mov $0, %eax
+    # i += 1
+    incl %edx
+
+    jmp _for_each_char
+
+
 _exit:
-    // SYSCALL exit (0)
+    # syscall exit (0)
     mov $1, %eax
     xor %ebx, %ebx
     int $0x80
 
-
 _panic:
-    // SYSCALL close (fd)
-    mov $6, %eax
-    mov fd, %ebx
-    int $0x80
-
-    // SYSCALL exit (1)
+    # syscall exit (1)
     mov $1, %eax
     mov $1, %ebx
     int $0x80
 
-_start:
-    jmp _open
